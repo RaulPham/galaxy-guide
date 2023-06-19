f6c9a958fc2470ba1fd5f917f9edc80f2ba1cc6c
Raul Pham <phatpham314@icloud.com>
2023-06-19 14:36:29 -0500

    Initial Commit

---
+++ GalaxyGuide/GalaxyGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyGuideApp.swift
+//  GalaxyGuide
+//
+//  Created by Pham on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
